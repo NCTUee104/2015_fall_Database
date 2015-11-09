92b46f72a69343d1b4c4cb4dcd7e79fee46de96b
Edward <w22116972@gmail.com>
2015-11-09 16:25:20 +0800

    Update hw4.java

--- hw4.java
+++ hw4.java
@@ -2,7 +2,7 @@
  * Created by Edward on 2015/11/9.
  *
  * @author Edward
- * @version 1.0
+ * @version 2.0
  * @since   2015-11-09
  * Default Interface is Public.
  */
@@ -10,8 +10,8 @@ interface Calculator {
     /**
      * Sum long.
      *
-     * @param r which row number needed to use.
-     * @return  sum value of long type.
+     * @param   r which row number needed to use.
+     * @return    sum value of long type.
      *
      * Default methods in Interface are all Public and Abstract.
     */
@@ -20,8 +20,8 @@ interface Calculator {
     /**
      * Avg double.
      *
-     * @param r which row number needed to use.
-     * @return  average value of double type.
+     * @param   r which row number needed to use.
+     * @return    average value of double type.
      *
      * Default methods in Interface are all Public and Abstract.
      */
@@ -45,19 +45,20 @@ public class Progression2_0450742 extends Progression_0450742 implements Calcula
     /**
      * Return sum value of given row.
      *
-     * @param r specify which row needed to use.
-     * @return  sum value
-     * @throws  ArithmeticException If adding process is overflow.
+     * @param   r                              specify which row needed to use.
+     * @return                                 sum value
+     * @throws  ArithmeticException            If adding process is overflow.
+     * @throws  ArrayIndexOutOfBoundsException If index is out of bounds.
      */
     public final long sum(final int r) {
         if (r < 0 || r >= super.returnArray().length) {
-            throw new ArrayIndexOutOfBoundsException();
+            throw new ArrayIndexOutOfBoundsException("Index must be >= 0 or < array length.");
         }
         long ans = 0;
         long[] row = super.returnRow(r);
         for (long i : row) {
             if (ans + i < ans) {
-                throw new ArithmeticException("Overflow occur");
+                throw new ArithmeticException("Arithmetic overflow.");
             } else {
                 ans += i;
             }
@@ -68,19 +69,20 @@ public class Progression2_0450742 extends Progression_0450742 implements Calcula
     /**
      * Return average value of all elements in given row.
      *
-     * @param r specify which row needed to use.
-     * @return  average value.
-     * @throws  ArithmeticException If adding process is overflow.
+     * @param   r                              specify which row needed to use.
+     * @return                                 average value.
+     * @throws  ArithmeticException            If adding process is overflow.
+     * @throws  ArrayIndexOutOfBoundsException If index is out of bounds.
      */
     public final double avg(final int r) {
         if (r < 0 || r >= super.returnArray().length) {
-            throw new ArrayIndexOutOfBoundsException("Index must be < 1 or ");
+            throw new ArrayIndexOutOfBoundsException("Index must be >= 0 or < array length.");
         }
         double ans = 0;
         long[] row = super.returnRow(r);
         for (long i : row) {
             if (ans + i < ans) {
-                throw new ArithmeticException("Overflow occur");
+                throw new ArithmeticException("Arithmetic overflow.");
             } else {
                 ans += i;
             }
